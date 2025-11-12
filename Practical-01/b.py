b00303aa36b77b5aa201ad326976714cfa2394fe
Karan Kumar <karankumarff17@gmail.com>
2025-11-12 20:59:58 +0530

    Implement process forking with child and parent output

--- Practical-01/b.py
+++ Practical-01/b.py
@@ -1,1 +1,12 @@
+import os
 
+pid = os.fork()
+
+if pid == 0:
+    print(f"Child Process running code block | PID: {os.getpid()}")
+    for i in range(5):
+        print(f"Child counting: {i+1}")
+else:
+    print(f"Parent Process running code block | PID: {os.getpid()}")
+    for i in range(3):
+        print(f"Parent counting: {i+1}")
